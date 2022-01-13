595e16509a27e74a978d9b7bfef03db1f21830f1
zkk-pro <iamkunya@163.com>
2022-01-13 16:09:19 +0800

    Update Tutorial.vue
    center

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,7 +1,7 @@
 <!-- Please remove this file from your project -->
 <template>
   <div
-    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+    class="relative flex items-center justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
   >
     <link
       href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
